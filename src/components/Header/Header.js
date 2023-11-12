4097d2f84aa9df70b9e8808b0acba9f55cac8b53
randomu3 <demiz99@mail.ru>
2023-11-12 23:23:56 +0500

    stable header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,70 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, useMediaQuery } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Menu from './Menu';
 import FundMenuItems from '../../utils/Menu/FundMenu';
 import ProgramsMenuItems from '../../utils/Menu/ProgramsMenuItems';
 import PerformanceIndicatorsMenuItems from '../../utils/Menu/PerformanceIndicatorsMenuItems';
 import ClientsNpfMenuItems from '../../utils/Menu/ClientsNpfMenuItems';
+import { useTheme } from '@mui/material/styles';
+import Link from 'next/link';
 
 const Header = () => {
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
+                {!isDesktop && (
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                )}
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     НПФ Сургутнефтегаз
                 </Typography>
-                <Menu title="О Фонде" items={FundMenuItems} />
-                <Menu title="Программы" items={ProgramsMenuItems} />
-                <Menu title="Показатели деятельности" items={PerformanceIndicatorsMenuItems} />
-                <Menu title="Клиентам НПФ" items={ClientsNpfMenuItems} />
-                <Button color="inherit">Личный Кабинет</Button>
-                <Button color="inherit">Обратиться в Фонд</Button>
+                {isDesktop && (
+                    <>
+                        <Menu title="О Фонде" items={FundMenuItems} />
+                        <Menu title="Клиентам НПФ" items={ClientsNpfMenuItems} />
+                        <Menu title="Программы" items={ProgramsMenuItems} />
+                        <Menu title="Показатели деятельности" items={PerformanceIndicatorsMenuItems} />
+                    </>
+                )}
+                <Link href="/personal-account" passHref>
+                    <Button
+                        color="inherit"
+                        sx={{
+                            color: 'white',
+                            backgroundColor: 'primary.main',
+                            '&:hover': {
+                                backgroundColor: 'primary.dark',
+                            },
+                        }}
+                    >
+                        Личный Кабинет
+                    </Button>
+                </Link>
+                <Link href="/contact-fund" passHref>
+                    <Button
+                        color="inherit"
+                        sx={{
+                            color: 'white',
+                            backgroundColor: 'primary.main',
+                            '&:hover': {
+                                backgroundColor: 'primary.dark',
+                            },
+                        }}
+                    >
+                        Обратиться в Фонд
+                    </Button>
+                </Link>
             </Toolbar>
         </AppBar>
     );
--- src/utils/Menu/ProgramsMenuItems.js
+++ src/utils/Menu/ProgramsMenuItems.js
@@ -1,20 +1,10 @@
 const ProgramsMenuItems = [
-    {
-        title: "Для физических лиц",
-        items: [
-            { title: "Негосударственное пенсионное обеспечение", href: "/individuals/pension-provision" },
-            { title: "Обязательное пенсионное страхование", href: "/individuals/mandatory-pension-insurance" },
-            { title: "Программа «Софинансирование»", href: "/individuals/co-financing" },
-            { title: "Программа «Партнерство»", href: "/individuals/partnership" },
-            { title: "Для обязательного сведения", href: "/individuals/mandatory-information" }
-        ]
-    },
-    {
-        title: "Для юридических лиц",
-        items: [
-            { title: "Информация для юридических лиц", href: "/legal-entities/information" }
-        ]
-    }
+    { title: "Негосударственное пенсионное обеспечение", href: "/individuals/pension-provision" },
+    { title: "Обязательное пенсионное страхование", href: "/individuals/mandatory-pension-insurance" },
+    { title: "Программа «Софинансирование»", href: "/individuals/co-financing" },
+    { title: "Программа «Партнерство»", href: "/individuals/partnership" },
+    { title: "Для обязательного сведения", href: "/individuals/mandatory-information" },
+    { title: "Информация для юридических лиц", href: "/legal-entities/information" }
 ];
 
 export default ProgramsMenuItems;
